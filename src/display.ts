58468393727a6c5ca942c7be91167fd168bbfbac
Glenn Vandeuren <vandeurenglenn@gmail.com>
2023-06-16 20:28:57 +0200

    wrap-center: html -> styles

--- src/display.ts
+++ src/display.ts
@@ -1,4 +1,4 @@
-import { LitElement, html, css } from "lit";
+import { LitElement, html, css, CSSResultGroup } from "lit";
 import { property } from "lit/decorators.js";
 
 /**
--- src/wrap-center.ts
+++ src/wrap-center.ts
@@ -1,20 +1,19 @@
-import { html } from "lit";
+import { CSSResultGroup, css, html } from "lit";
 import { FlexDisplay } from "./display.js";
 
 export class FlexWrapCenter extends FlexDisplay {
   constructor() {
     super();
   }
-  render() {
-    return html`<style>
-      :host {
-        display: flex;
-        flex-flow: row wrap;
-        --flex-display-justify-content: center;
-      }      
-    </style>
-    <slot></slot>
+
+  static styles?: CSSResultGroup = [
+    css`
+    :host {
+      display: flex;
+      flex-flow: row wrap;
+      --flex-display-justify-content: center;
+    }
     `
-  }
+  ]
 }
 customElements.define('flex-wrap-center', FlexWrapCenter);
